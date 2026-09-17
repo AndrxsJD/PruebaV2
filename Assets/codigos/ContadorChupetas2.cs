5037ac11c72395a612ee18d3e6c3bef39ec6f2f9
AndrxsJD <andresguardo2476@gmail.com>
2026-09-17 16:07:49 -0500

    aaa

--- Assets/codigos/ContadorChupetas2.cs
+++ Assets/codigos/ContadorChupetas2.cs
@@ -1,6 +1,49 @@
 using UnityEngine;
 
-public class ContadorChupetas2 : MonoBehaviour
+public class ContadorChupetas2 : MonoBehaviour, IRecolectables1
 {
+    public void AumentarContador()
+    {
+
+    }
+
+    public void AumentarContador(int _cantidad)
+    {
+
+    }
+
+    public void DisminuirContador()
+    {
+
+    }
+
+    public void DisminuirContador(int _cantidad)
+    {
+
+    }
+    public void IniciarContador()
+    {
+
+    }
+
+    public void IniciarContador(int _cantidad)
+    {
+
+    }
     
+
+    public void AlIniciarScript()
+    {
+
+    }
+
+    public void AlActivarScript()
+    {
+
+    }
+
+    public void AlDesactivarScript()
+    {
+
+    }
 }
--- Assets/codigos/IRecolectables1.cs
+++ Assets/codigos/IRecolectables1.cs
@@ -2,10 +2,14 @@
 public interface IRecolectables1
 {
     void IniciarContador();
+    void IniciarContador(int _cantidad);
     void AumentarContador();
+    void AumentarContador(int _cantidad);
     void DisminuirContador();
+    void DisminuirContador(int _cantidad);
+
 
     void AlIniciarScript();
     void AlActivarScript();
-    void AlDesactivarScreipt();
+    void AlDesactivarScript();
 }
